3f44b86d45a08ee41f2f5de33439d79c57a167f4
shiotomo <t.shiota16@gmail.com>
2018-04-25 16:41:23 +0900

    rpromptの設定を変更した

--- zsh/.zsh.d/rprompt.zsh
+++ zsh/.zsh.d/rprompt.zsh
@@ -8,4 +8,4 @@ zstyle ':vcs_info:*' formats "%F{green}%c%u[%b]%f"
 zstyle ':vcs_info:*' actionformats '[%b|%a]'
 precmd () { vcs_info }
 
-RPROMPT=$RPROMPT'${vcs_info_msg_0_}%F{white}[$?]%f'
+RPROMPT='${vcs_info_msg_0_}%F{white}[$?]%f'
