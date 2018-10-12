681096f2147a6dc649104e92be057db3c08971ca
shiotomo <t.shiota16@gmail.com>
2018-10-13 01:46:12 +0900

    Add raspbian

---
+++ raspbian/installer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get install -y vim
+sudo apt-get install -y ruby
+sudo apt-get install -y python3
+sudo apt-get install -y gcc
+sudo apt-get install -y nodejs
+sudo apt-get install -y npm
+sudo apt-get install -y zsh
+sudo apt-get install -y tmux
