88d6f2a9ca8fd7cf6df1457e5fb619523a64d659
shiotomo <t.shiota16@gmail.com>
2018-11-11 00:07:39 +0900

    インストールするvimの設定を変更した

--- ubuntu/start.sh
+++ ubuntu/start.sh
@@ -40,7 +40,7 @@ cd /opt/
 sudo git clone https://github.com/vim/vim 
 cd vim/
 
-sudo ./configure --with-features=huge --enable-multibyte --enable-luainterp=dynamic --enable-gpm --enable-cscope --enable-fontset --enable-fail-if-missing --prefix=/usr/local --enable-pythoninterp=dynamic --enable-lpython3interp=dynamic --enable-rubyinterp=dynamic 
+sudo ./configure --with-features=huge --enable-multibyte --enable-luainterp=dynamic --enable-gpm --enable-cscope --enable-fontset --enable-fail-if-missing --prefix=/usr/local --enable-pythoninterp=dynamic --enable-lpython3interp=dynamic --enable-rubyinterp=dynamic --enable-python3interp
 
 sudo make
 sudo make install
