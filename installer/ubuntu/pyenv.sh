40da92018c7ce6c5339774c563a1944fe6eb787c
shiotomo <t.shiota16@gmail.com>
2019-02-10 02:26:14 +0900

    pyenv.shを変更した

--- installer/ubuntu/pyenv.sh
+++ installer/ubuntu/pyenv.sh
@@ -10,5 +10,3 @@ sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
 libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils
 
 git clone https://github.com/yyuu/pyenv.git ~/.pyenv
-
-exec $SHELL
