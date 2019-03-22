e4ef2c88b096bd77d394aae77ff9a38c69a0ebc0
shiotomo <t.shiota16@gmail.com>
2019-03-22 20:28:05 +0000

    Update mygit.sh

--- installer/mygit.sh
+++ installer/mygit.sh
@@ -2,9 +2,7 @@
 
 cd
 git clone https://github.com/junegunn/fzf.git ~/.fzf
-mkdir .vim
-cd .vim
-git clone https://github.com/shiotomo/.vim.git ~/.vim
+git clone git@github.com:shiotomo/.vim.git ~/.vim
 
 cd
 ln -s ~/.dotfiles/zsh/.zshrc
