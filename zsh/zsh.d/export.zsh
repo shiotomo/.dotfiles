158509129643889d8168285c3016373480b3c982
shiotomo <t.shiota16@gmail.com>
2019-04-13 01:13:48 +0900

    Update export.zsh

--- zsh/zsh.d/export.zsh
+++ zsh/zsh.d/export.zsh
@@ -2,6 +2,9 @@ export LANG=ja_JP.UTF-8
 export LSCOLORS=bxfxcxdxbxegedabagacad
 export PATH="$HOME/bin:$PATH"
 export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
+export PATH="/usr/local/opt/openssl/bin:$PATH"
+export LDFLAGS="-L/usr/local/opt/openssl/lib"
+export CPPFLAGS="-I/usr/local/opt/openssl/include"
 
 # == Editor ==
 export EDITOR='vim'
