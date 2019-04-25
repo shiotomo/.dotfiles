a0e642355d22630673389f15132578dfd2f76885
shiotomo <t.shiota16@gmail.com>
2019-04-26 06:21:38 +0900

    Dockerのバージョンを更新した

--- installer/ubuntu/java.sh
+++ installer/ubuntu/java.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # === Java8 ===
-# sudo add-apt-repository ppa:webupd8team/java
-# sudo apt-get update
-# sudo apt-get install oracle-java8-installer
-
-sudo add-apt-repository ppa:linuxuprising/java
+sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get install oracle-java10-installer
+sudo apt-get install oracle-java8-installer
+
+# sudo add-apt-repository ppa:linuxuprising/java
+# sudo apt-get update
+# sudo apt-get install oracle-java10-installer
