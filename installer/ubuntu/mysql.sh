bd1c415ecfd688d84830427eb8ddee5a9ed7cf59
shiotomo <t.shiota16@gmail.com>
2019-09-30 02:59:33 +0900

    [installer] ubuntuのinstallerを更新した

--- installer/ubuntu/mysql.sh
+++ installer/ubuntu/mysql.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
+curl -o $HOME/.dotfiles/tmp/mysql.deb https://repo.mysql.com/mysql-apt-config_0.8.10-1_all.deb
+sudo dpkg -i $HOME/.dotfiles/tmp/mysql.deb
 sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get install -y libmysqlclient-dev
-sudo apt-get install -y mysql-server mysql-client
+sudo apt-get install -y mysql-server mysql-client libmysqlclient-dev
--- installer/ubuntu/start.sh
+++ installer/ubuntu/start.sh
@@ -45,7 +45,7 @@ sudo rm /usr/local/bin/vim
 sudo apt-get install -y git build-essential ncurses-dev lua5.2 lua5.2-dev luajit python-dev python3-dev ruby-dev
 
 cd /opt/
-sudo git clone https://github.com/vim/vim 
+sudo git clone https://github.com/vim/vim
 cd vim/
 
 sudo ./configure --with-features=huge --enable-multibyte --enable-luainterp=dynamic --enable-gpm --enable-cscope --enable-fontset --enable-fail-if-missing --prefix=/usr/local --enable-pythoninterp=dynamic --enable-lpython3interp=dynamic --enable-rubyinterp=dynamic --enable-python3interp
