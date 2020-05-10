d762b71783a44c1f3d6886d9fa198420ef0b8e21
shiotomo <t.shiota16@gmail.com>
2020-05-11 01:35:39 +0900

    [mac] brew.shを更新した

--- installer/macos/brew.sh
+++ installer/macos/brew.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
 brew cask install adoptopenjdk
 # brew cask install google-chrome
 # brew cask install vagrant
