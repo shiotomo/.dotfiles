ba08a965fb4070c2a3921ef127abe3ef6403cb43
shiotomo <t.shiota16@gmail.com>
2020-05-15 16:37:55 +0900

    [zsh] promptの表示を変更した

--- zsh/zsh.d/function.zsh
+++ zsh/zsh.d/function.zsh
@@ -20,3 +20,20 @@ function gi() {
 function texc() {
   docker run --rm -v `pwd`:/workdir paperist/alpine-texlive-ja ptex2pdf -l $1
 }
+
+function localip() {
+  case ${OSTYPE} in
+    darwin*)
+      # MacOS用の設定
+      ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'
+      ;;
+    linux*)
+      # Linux用の設定
+      hostname -I | cut -f1 -d' '
+      ;;
+    cygwin*)
+      # cygwin用の設定
+      hostname -I | cut -f1 -d' '
+      ;;
+  esac
+}
--- zsh/zsh.d/prompt.zsh
+++ zsh/zsh.d/prompt.zsh
@@ -1,16 +1,18 @@
-case ${OSTYPE} in
-  darwin*)
-    # MacOS用の設定
-    ip=`ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'`
-    ;;
-  linux*)
-    # Linux用の設定
-    ip=`hostname -I | cut -f1 -d' '`
-    ;;
-  cygwin*)
-    # cygwin用の設定
-    ip=`hostname -I | cut -f1 -d' '`
-    ;;
-esac
-PROMPT='%F{cyan}[%n@%m:%f%F{yello}%~%f%F{cyan}]%f %F{green}[$ip]%f
+# case ${OSTYPE} in
+#   darwin*)
+#     # MacOS用の設定
+#     ip=`ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'`
+#     ;;
+#   linux*)
+#     # Linux用の設定
+#     ip=`hostname -I | cut -f1 -d' '`
+#     ;;
+#   cygwin*)
+#     # cygwin用の設定
+#     ip=`hostname -I | cut -f1 -d' '`
+#     ;;
+# esac
+# PROMPT='%F{cyan}[%n@%m:%f%F{yello}%~%f%F{cyan}]%f %F{green}[$ip]%f
+# %B%(?,%F{magenta},%F{red})%(!,#,>>)%b '
+PROMPT='%F{cyan}[%n@%m:%f%F{yello}%~%f%F{cyan}]%f
 %B%(?,%F{magenta},%F{red})%(!,#,>>)%b '
