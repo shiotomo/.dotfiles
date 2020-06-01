b62c60ced2cb460b71d0d93d0e232dba272618b7
shiotomo <t.shiota16@gmail.com>
2020-06-01 09:15:40 +0900

    ファイル名を変更した

--- installer/mygit.sh
+++ installer/mygit.sh
@@ -9,12 +9,13 @@ git clone git@github.com:shiotomo/aquarium.git ~/aquarium
 # tmux plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
-ln -s ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
-ln -s ~/.dotfiles/vim/.vimrc ~/.vimrc
-ln -s ~/.dotfiles/vim/.vimshrc ~/.vimshrc
-ln -s ~/.dotfiles/.inputrc ~/.inputrc
-ln -s ~/.dotfiles/eclipse ~/.vrapperrc
+ln -s ~/.dotfiles/zsh/zshrc ~/.zshrc
+ln -s ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
+ln -s ~/.dotfiles/vim/vimshrc ~/.vimshrc
+ln -s ~/.dotfiles/inputrc ~/.inputrc
+ln -s ~/.dotfiles/gemrc ~/.gemrc
+ln -s ~/.dotfiles/eclipse/vrapperrc ~/.vrapperrc
 
 mkdir ~/.dotfiles/vim/bundle
 mkdir ~/.dotfiles/vim/help
