bb3a76abe4cfe5d843e033d5be22734663cb74ad
shiotomo <t.shiota16@gmail.com>
2020-06-28 14:44:33 +0900

    [installer] mygit.shを更新した

--- installer/mygit.sh
+++ installer/mygit.sh
@@ -9,6 +9,7 @@ git clone git@github.com:shiotomo/aquarium.git ~/aquarium
 # tmux plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+# Set .dotfiles symbolic link
 ln -s ~/.dotfiles/zsh/zshrc ~/.zshrc
 ln -s ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
@@ -16,7 +17,9 @@ ln -s ~/.dotfiles/vim/vimshrc ~/.vimshrc
 ln -s ~/.dotfiles/inputrc ~/.inputrc
 ln -s ~/.dotfiles/gemrc ~/.gemrc
 ln -s ~/.dotfiles/eclipse/vrapperrc ~/.vrapperrc
+ln -s ~/.dotfiles/intelliJ_IDEA/ideavimrc ~/.ideavimrc
 
+# Vim setting
 mkdir ~/.dotfiles/vim/bundle
 mkdir ~/.dotfiles/vim/help
 
