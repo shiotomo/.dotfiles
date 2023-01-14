2329685e7dae18e5c80e0eb1b77422cdd6b570ff
shiotomo <t.shiota16@gmail.com>
2023-01-14 12:12:46 +0900

    [hammerspoon] subショートカットを追加

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,4 +1,5 @@
 hs.window.animationDuration = 0
+
 units = {
   left        = { x = 0.00, y = 0.00, w = 0.50, h = 1.00 },
   right       = { x = 0.50, y = 0.00, w = 0.50, h = 1.00 },
@@ -16,22 +17,45 @@ units = {
   centerRight = { x = 0.50, y = 0.25, w = 0.50, h = 0.50 },
 
   maximum     = { x = 0.00, y = 0.00, w = 1.00, h = 1.00 },
-  center      = { x = 0.25, y = 0.25, w = 0.50, h = 0.50 }
+  center      = { x = 0.25, y = 0.25, w = 0.50, h = 0.50 },
+
+  wideLeft    = { x = 0.00, y = 0.00, w = 0.66, h = 1.00 },
+  wideRight   = { x = 0.34, y = 0.00, w = 0.66, h = 1.00 },
+  wideTop     = { x = 0.00, y = 0.00, w = 1.00, h = 0.66 },
+  wideBotm    = { x = 0.00, y = 0.34, w = 1.00, h = 0.66 },
+
+  minLeft     = { x = 0.00, y = 0.00, w = 0.34, h = 1.00 },
+  minRight    = { x = 0.83, y = 0.00, w = 0.34, h = 1.00 },
+  minTop      = { x = 0.00, y = 0.00, w = 1.00, h = 0.50 },
+  minBotm     = { x = 0.00, y = 0.50, w = 1.00, h = 0.50 },
+
+  popupCenter = { x = 0.10, y = 0.10, w = 0.80, h = 0.80 },
+  wideCenter  = { x = 0.00, y = 0.10, w = 1.00, h = 0.80 },
 }
 
-mash = { 'option' }
-hs.hotkey.bind(mash, 'H',     function() hs.window.focusedWindow():move(units.left,        nil, true) end)
-hs.hotkey.bind(mash, 'L',     function() hs.window.focusedWindow():move(units.right,       nil, true) end)
-hs.hotkey.bind(mash, 'K',     function() hs.window.focusedWindow():move(units.top,         nil, true) end)
-hs.hotkey.bind(mash, 'J',     function() hs.window.focusedWindow():move(units.botm,        nil, true) end)
-hs.hotkey.bind(mash, '1',     function() hs.window.focusedWindow():move(units.leftTop,     nil, true) end)
-hs.hotkey.bind(mash, '2',     function() hs.window.focusedWindow():move(units.leftBotm,    nil, true) end)
-hs.hotkey.bind(mash, '3',     function() hs.window.focusedWindow():move(units.rightTop,    nil, true) end)
-hs.hotkey.bind(mash, '4',     function() hs.window.focusedWindow():move(units.rightBotm,   nil, true) end)
-hs.hotkey.bind(mash, 'up',    function() hs.window.focusedWindow():move(units.centerTop,   nil, true) end)
-hs.hotkey.bind(mash, 'down',  function() hs.window.focusedWindow():move(units.centerBotm,  nil, true) end)
-hs.hotkey.bind(mash, 'left',  function() hs.window.focusedWindow():move(units.centerLeft,  nil, true) end)
-hs.hotkey.bind(mash, 'right', function() hs.window.focusedWindow():move(units.centerRight, nil, true) end)
-hs.hotkey.bind(mash, 'm',     function() hs.window.focusedWindow():move(units.maximum,     nil, true) end)
-hs.hotkey.bind(mash, 'c',     function() hs.window.focusedWindow():move(units.center,      nil, true) end)
+mainHotKey = { 'option' }
+hs.hotkey.bind(mainHotKey, 'h',     function() hs.window.focusedWindow():move(units.left,        nil, true) end)
+hs.hotkey.bind(mainHotKey, 'l',     function() hs.window.focusedWindow():move(units.right,       nil, true) end)
+hs.hotkey.bind(mainHotKey, 'k',     function() hs.window.focusedWindow():move(units.top,         nil, true) end)
+hs.hotkey.bind(mainHotKey, 'j',     function() hs.window.focusedWindow():move(units.botm,        nil, true) end)
+hs.hotkey.bind(mainHotKey, '1',     function() hs.window.focusedWindow():move(units.leftTop,     nil, true) end)
+hs.hotkey.bind(mainHotKey, '2',     function() hs.window.focusedWindow():move(units.leftBotm,    nil, true) end)
+hs.hotkey.bind(mainHotKey, '3',     function() hs.window.focusedWindow():move(units.rightTop,    nil, true) end)
+hs.hotkey.bind(mainHotKey, '4',     function() hs.window.focusedWindow():move(units.rightBotm,   nil, true) end)
+hs.hotkey.bind(mainHotKey, 'up',    function() hs.window.focusedWindow():move(units.centerTop,   nil, true) end)
+hs.hotkey.bind(mainHotKey, 'down',  function() hs.window.focusedWindow():move(units.centerBotm,  nil, true) end)
+hs.hotkey.bind(mainHotKey, 'left',  function() hs.window.focusedWindow():move(units.centerLeft,  nil, true) end)
+hs.hotkey.bind(mainHotKey, 'right', function() hs.window.focusedWindow():move(units.centerRight, nil, true) end)
+hs.hotkey.bind(mainHotKey, 'm',     function() hs.window.focusedWindow():move(units.maximum,     nil, true) end)
+hs.hotkey.bind(mainHotKey, 'c',     function() hs.window.focusedWindow():move(units.center,      nil, true) end)
+
+subHotKey = { 'option', 'command' }
+hs.hotkey.bind(subHotKey, 'h',     function() hs.window.focusedWindow():move(units.wideLeft,     nil, true) end)
+hs.hotkey.bind(subHotKey, 'l',     function() hs.window.focusedWindow():move(units.wideRight,    nil, true) end)
+hs.hotkey.bind(subHotKey, 'k',     function() hs.window.focusedWindow():move(units.wideTop,     nil, true) end)
+hs.hotkey.bind(subHotKey, 'j',     function() hs.window.focusedWindow():move(units.wideBotm,    nil, true) end)
+hs.hotkey.bind(subHotKey, 'left',  function() hs.window.focusedWindow():move(units.minLeft,    nil, true) end)
+hs.hotkey.bind(subHotKey, 'right', function() hs.window.focusedWindow():move(units.minRight,   nil, true) end)
+hs.hotkey.bind(subHotKey, 'c',     function() hs.window.focusedWindow():move(units.popupCenter, nil, true) end)
+hs.hotkey.bind(subHotKey, 'm',     function() hs.window.focusedWindow():move(units.wideCenter,  nil, true) end)
 
