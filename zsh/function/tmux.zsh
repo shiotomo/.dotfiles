26f77c5b470612c0ca7980cf3471148c1711608b
shiotomo <t.shiota16@gmail.com>
2023-01-16 22:12:37 +0900

    [zsh] dotfilesをtmuxで起動できるコマンドを実装した

---
+++ zsh/function/tmux.zsh
@@ -0,0 +1,11 @@
+dotfiles() {
+  cd ~/.dotfiles
+  RESULT=(`tmux ls | grep -E "^dotfiles:"`)
+  if [ -n "$TMUX" ]; then
+    tmux switch -t dotfiles
+  elif [ -n "$RESULT" ]; then
+    tmux a -t dotfiles
+  else
+    tmux new-session -s dotfiles
+  fi
+}
