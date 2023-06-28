f2801b33e7056fabdfc6dc20de227b13af433be4
shiotomo <t.shiota16@gmail.com>
2023-06-28 22:53:20 +0900

    [zsh] twsコマンドを修正

--- zsh/function/file.zsh
+++ zsh/function/file.zsh
@@ -33,11 +33,17 @@ tws() {
   local dir=$(cat $HOME/.config/zsh/workspace.list | fzf)
   local dir_name=`basename ${dir}`
   RESULT=(`tmux ls | grep -E "^$dir_name:"`)
-  if [ -n "$TMUX" ]; then
+  if [ -n "$TMUX" ] && [ -n "$RESULT" ]; then
     tmux switch -t $dir_name
+  elif [ -n "$TMUX" ]; then
+    echo "以下のコマンドを tmux detach した後実行してください。"
+    echo "cd $dir_name && tmux new-session -s $dir_name"
+    echo "cd $dir_name && tmux new-session -s $dir_name" | clip
+    echo "上記のコマンドを clip boardにコピーしました。"
   elif [ -n "$RESULT" ]; then
     tmux a -t $dir_name
   else
+    cd $dir_name
     tmux new-session -s $dir_name
   fi
 }
--- zsh/function/tmux.zsh
+++ zsh/function/tmux.zsh
@@ -2,11 +2,11 @@ dotfiles() {
   cd ~/.dotfiles
   RESULT=(`tmux ls | grep -E "^dotfiles:"`)
   if [ -n "$TMUX" ]; then
-    tmux switch -t dotfiles
+    tmux switch -t _dotfiles
   elif [ -n "$RESULT" ]; then
-    tmux a -t dotfiles
+    tmux a -t _dotfiles
   else
-    tmux new-session -s dotfiles
+    tmux new-session -s _dotfiles
   fi
 }
 
