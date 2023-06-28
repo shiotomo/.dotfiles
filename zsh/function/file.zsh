57fe21183abea650f894678579fceffd0dd34539
shiotomo <t.shiota16@gmail.com>
2023-06-28 23:33:17 +0900

    [zsh] twsコマンドを改良

--- zsh/function/file.zsh
+++ zsh/function/file.zsh
@@ -24,26 +24,35 @@ ws() {
   cd $dir
 }
 
-# .config/zsh/workspace.listに設定したディレクトリに移動するためのコマンド
+# .config/zsh/workspace.listを編集するためのコマンド
 wse() {
   vim $HOME/.config/zsh/workspace.list
 }
 
+# .config/zsh/workspace.listに現在のパスを追加するコマンド
+ws-add() {
+  pwd >> $HOME/.config/zsh/workspace.list
+}
+
+# .config/zsh/workspace.listに設定したディレクトリでtmuxを起動する小アンド
 tws() {
   local dir=$(cat $HOME/.config/zsh/workspace.list | fzf)
   local dir_name=`basename ${dir}`
-  RESULT=(`tmux ls | grep -E "^$dir_name:"`)
+  local tmux_session_name=`(basename $dir | sed -e 's/^./_/g')`
+  RESULT=(`tmux ls | grep -E "^$tmux_session_name:"`)
   if [ -n "$TMUX" ] && [ -n "$RESULT" ]; then
-    tmux switch -t $dir_name
+    tmux switch -t $tmux_session_name
   elif [ -n "$TMUX" ]; then
     echo "以下のコマンドを tmux detach した後実行してください。"
-    echo "cd $dir_name && tmux new-session -s $dir_name"
-    echo "cd $dir_name && tmux new-session -s $dir_name" | clip
+    # echo "cd $dir_name && tmux new-session -s $dir_name"
+    # echo "cd $dir_name && tmux new-session -s $dir_name" | clip
+    echo "tmux new-session -s $tmux_session_name"
+    echo "tmux new-session -s $tmux_session_name" | clip
     echo "上記のコマンドを clip boardにコピーしました。"
   elif [ -n "$RESULT" ]; then
-    tmux a -t $dir_name
+    tmux a -t $tmux_session_name
   else
-    cd $dir_name
-    tmux new-session -s $dir_name
+    cd (basename $dir)
+    tmux new-session -s $tmux_session_name
   fi
 }
