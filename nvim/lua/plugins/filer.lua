555f260e4ceab9567048edf963e2f20cc1513e81
shiotomo <t.shiota16@gmail.com>
2023-08-23 01:33:17 +0900

    [nvim] フォルダ分割の構成を変更

--- nvim/lua/plugins/filer.lua
+++ nvim/lua/plugins/filer.lua
@@ -1,5 +1,5 @@
+-- nerdtree
 vim.g.NERDTreeShowHidden = true
-
 vim.keymap.set('n', '<C-e>', '<cmd>NERDTreeToggle<CR>', { silent = true })
 
 -- telescope
---
+++ nvim/lua/plugins/status_line.lua
@@ -0,0 +1,44 @@
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = {
+      'branch',
+      'diff',
+      {
+        'diagnostics',
+        sources = {
+          'nvim_lsp'
+        },
+        symbols = {
+          error = ' ',
+          warn  = ' ',
+          info  = ' ',
+          hint  = ' '
+        },
+      }
+    },
+    lualine_z = { 'location' }
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+}
