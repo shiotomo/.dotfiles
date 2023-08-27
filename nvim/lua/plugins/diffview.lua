1ecfa2cd6ba2e258c231fe967970e67935d92bc3
shiotomo <t.shiota16@gmail.com>
2023-08-27 13:37:43 +0900

    [nvim] diffviewの設定を追加

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -1,3 +1,5 @@
 -- vim.keymap.set('n', '<leader>r', '<cmd>DiffviewOpen<CR>')
 -- vim.keymap.set('n', '<leader>d', '<cmd>DiffviewClose<CR>')
 
+vim.cmd("command! ReviewOpen DiffviewOpen")
+vim.cmd("command! ReviewClose DiffviewClose")
