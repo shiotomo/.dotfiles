0561f3b09248222f6435683a6ca73c281ec50838
shiotomo <t.shiota16@gmail.com>
2023-08-27 13:34:17 +0900

    [nvim] terminal周りの設定を追加

--- nvim/lua/plugins/terminal.lua
+++ nvim/lua/plugins/terminal.lua
@@ -6,3 +6,6 @@ vim.keymap.set('n', '<leader>t', '<cmd>ToggleTerm direction=float<CR>')
 vim.keymap.set('n', '<leader>h', '<cmd>ToggleTerm direction=horizontal<CR>')
 vim.keymap.set('t', '<esc>', [[<C-\><C-n>]])
 
+vim.cmd("command! FloatT ToggleTerm direction=float")
+vim.cmd("command! HorT ToggleTerm direction=horizontal")
+vim.cmd("command! TabT ToggleTerm direction=tab")
