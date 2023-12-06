fc8a5bb92cf4ee2cea2a7cf701270ff8308e3dc7
shiotomo <t.shiota16@gmail.com>
2023-12-07 03:31:17 +0900

    [hammerspoon] toggleAppを作成

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -69,3 +69,17 @@ hs.hotkey.bind({ "command", "shift" }, "t", function()
   end
 end)
 
+local function toggleApp(appName, key)
+  hs.hotkey.bind({"option"}, key, function()
+    local app = hs.application.get(appName)
+    if app == nil then
+      hs.application.launchOrFocus("/Applications/" .. appName .. ".app")
+    elseif app:isFrontmost() then
+      app:hide()
+    else
+      hs.application.launchOrFocus("/Applications/" .. appName .. ".app")
+    end
+  end)
+end
+
+toggleApp("WezTerm", "space")
