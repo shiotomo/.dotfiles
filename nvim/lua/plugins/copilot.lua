710d0572c79fa983a794ec4abb15a63fe91edcf9
shiotomo <t.shiota16@gmail.com>
2024-01-21 05:06:55 +0900

    copilotの設定を追加

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,8 @@
+return {
+  "github/copilot.vim",
+  lazy=false,
+  config = function ()
+    vim.g.copilot_no_tab_map = true
+    vim.api.nvim_set_keymap("i", "<C-a>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+  end
+}
