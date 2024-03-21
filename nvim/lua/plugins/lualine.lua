fd8f54638b7ee0e6a46a65acf822bfd4e56cb9d2
shiotomo <t.shiota16@gmail.com>
2024-03-22 01:21:13 +0900

    [nvim] 文字数カウントの設定を追加

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -4,6 +4,36 @@ return {
   lazy = true,
   event = "UIEnter",
   config = function()
+    local function selectionCount()
+      local mode = vim.fn.mode()
+      local start_line, end_line, start_pos, end_pos
+
+      -- 選択モードでない場合には無効
+      if not (mode:find("[vV\22]") ~= nil) then return "" end
+      start_line = vim.fn.line("v")
+      end_line = vim.fn.line(".")
+
+      if mode == 'V' then
+        -- 行選択モードの場合は、各行全体をカウントする
+        start_pos = 1
+        end_pos = vim.fn.strlen(vim.fn.getline(end_line)) + 1
+      else
+        start_pos = vim.fn.col("v")
+        end_pos = vim.fn.col(".")
+      end
+
+      local chars = 0
+      for i = start_line, end_line do
+        local line = vim.fn.getline(i)
+        local line_len = vim.fn.strlen(line)
+        local s_pos = (i == start_line) and start_pos or 1
+        local e_pos = (i == end_line) and end_pos or line_len + 1
+        chars = chars + vim.fn.strchars(line:sub(s_pos, e_pos - 1))
+      end
+
+      local lines = math.abs(end_line - start_line) + 1
+      return tostring(lines) .. " lines, " .. tostring(chars) .. " characters"
+    end
     require('lualine').setup {
       options = {
         icons_enabled = true,
@@ -40,6 +70,10 @@ return {
               hint  = ' '
             },
           }
+        },
+        lualine_x = {
+          {'searchcount'},
+          {selectionCount},
         },
         lualine_z = { 'location' }
       },
