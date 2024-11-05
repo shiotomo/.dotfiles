d303e6aa652ea4c99039865e4c2534aa9e57848f
shiotomo <t.shiota16@gmail.com>
2024-11-05 23:25:42 +0900

    update

--- zsh/zsh.d/mise.zsh
+++ zsh/zsh.d/mise.zsh
@@ -2,7 +2,12 @@ case ${OSTYPE} in
   darwin*)
     # MacOS用の設定
     # eval "$(/usr/local/bin/mise activate zsh)"
-    eval "$(/opt/homebrew/bin/mise activate zsh)"
+    local arch=$(uname -m)
+    if [ $arch = "x86_64" ]; then
+      eval "$(/usr/local/bin/mise activate zsh)"
+    else
+      eval "$(/opt/homebrew/bin/mise activate zsh)"
+    fi
     ;;
   linux*)
     # Linux用の設定
