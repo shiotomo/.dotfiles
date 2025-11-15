b92c629d4018b96f7ba6589e8aac75914d3d4e3c
shiotomo <t.shiota16@gmail.com>
2025-11-16 00:27:55 +0900

    [nvim] bufferline.nvimの設定を追加

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -5,28 +5,28 @@ return {
     dependencies = {
       'nvim-tree/nvim-web-devicons'
     },
-    config = Function ()
-    require("bufferline").setup({
-      options = {
-        mode = "tabs",         -- "buffers" or "tabs"
-        position = "bottom",      -- タブバーを下に配置（おしゃれ＆Neovimタブと干渉しない）
-        -- separator_style = "thick",
-        separator_style = "padded_slant",-- スラント境界
-        diagnostics = "nvim_lsp",
-        show_close_icon = true,
-        show_buffer_close_icons = true,
-        always_show_bufferline = true,
-        offsets = {
-          {
-            filetype = "nerdtree",
-            text = "File Explorer",
-            text_align = "left",
-            highlight = "Directory",
-            separator = true,
-          }
+    config = function ()
+      require("bufferline").setup({
+        options = {
+          mode = "tabs",         -- "buffers" or "tabs"
+          position = "bottom",      -- タブバーを下に配置（おしゃれ＆Neovimタブと干渉しない）
+          -- separator_style = "thick",
+          separator_style = "padded_slant",-- スラント境界
+          diagnostics = "nvim_lsp",
+          show_close_icon = true,
+          show_buffer_close_icons = true,
+          always_show_bufferline = true,
+          offsets = {
+            {
+              filetype = "nerdtree",
+              text = "File Explorer",
+              text_align = "left",
+              highlight = "Directory",
+              separator = true,
+            }
+          },
         },
-      },
-    })
-  end
+      })
+    end
   }
 }
