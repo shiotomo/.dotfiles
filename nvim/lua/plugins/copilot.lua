343b352af8459f4940dfaf9a33afd27151e8362d
shiotomo <t.shiota16@gmail.com>
2026-06-07 12:32:59 +0900

    fix copilot.lua

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,9 +1,10 @@
 return {
-
-  "github/copilot.vim",
-  lazy=false,
-  config = function ()
-    vim.g.copilot_no_tab_map = true
-    vim.api.nvim_set_keymap("i", "<C-a>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-  end
+  -- {
+  --   "github/copilot.vim",
+  --   lazy=false,
+  --   config = function ()
+  --     vim.g.copilot_no_tab_map = true
+  --     vim.api.nvim_set_keymap("i", "<C-a>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+  --   end
+  -- }
 }
